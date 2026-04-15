be25297d52f77e4106c2b5ed1cfc41ed4d82924e
Tadashi Hunter <tadashihunter07@gmail.com>
2026-04-15 12:35:21 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,22 +1,15 @@
 import requests
 import json
-
-name = input("Enter your city name: ")
+name=input("Enter your city name: ")
 api_key = "3dc951720c62bb3a109a320f24eaf5fc"
-
-url = f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={api_key}&units=metric"
-
+url = f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={api_key}"
 response = requests.get(url).json()
-
-if "weather" not in response:
-    print("API error:", response)
-else:
-    weather = response["weather"][0]["description"]
-    temp = response["main"]["temp"]
-
-    result = {
-        "weather": weather,
-        "temp": f"{temp:.2f}°C"
-    }
-
-    print(json.dumps(result, indent=2))
+weather=response["weather"][0]["description"]
+s=response['main']['temp']
+def convert_temperature(temperature,description):
+    e=float(temperature)-273.15
+    return json.dumps({
+        "weather":description,
+        "temp":f"{e:.2f}"
+    })
+print(convert_temperature(s,weather))
